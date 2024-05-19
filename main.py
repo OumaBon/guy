dc056b06c283e77a9bfef4530730a5f1e019e401
McMugah <blainelium@gmail.com>
2024-05-19 15:57:28 +0300

    initial setup of the current job

---
+++ main.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def json(self):
+        return{
+            "name": self.name,
+            "age": self.age,
+            "gender": self.gender
+        }
+
+student_1  = Student('Liam',45, 'male')
+print(student_1.name)
